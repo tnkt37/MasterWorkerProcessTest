7813eb60eda730de4ab5f41ed116412477e877b9
tnkt37 <tnkt37@outlook.jp>
2014-08-12 21:38:34 +0900

    master process read a file

---
+++ MasterWorkerProcessTest/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SecurityCamp
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void readFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(File.ReadAllText("testfile.txt"));
+        }
+    }
+}
