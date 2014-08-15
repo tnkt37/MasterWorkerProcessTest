7dc7d0b1d1854f03fa76d27ff34823b0157d166e
tnkt37 <tnkt37@outlook.jp>
2014-08-15 10:21:39 +0900

    set integrity level low

--- RestrectedTokenTest/Source.cpp
+++ RestrectedTokenTest/Source.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <sstream>
+#include <fstream>
+#include <iterator>
+
 #include <windows.h>
 #include <Sddl.h>
 
-//#define UP
+#define UP
 
 #ifdef UP
 #include "Python.h"
@@ -45,17 +48,50 @@ void OutputPirivileges(PTOKEN_PRIVILEGES pTokenPrivilegest)
 	}
 }
 
-void waitEnter()
+void waitEnter(const char message[] = "press enter...")
 {
+	cout << message << endl;
 	string dummmmm;
 	getline(cin, dummmmm);
+}
+
+void readFileTest(string fileName)
+{
+	std::stringstream ss;
+	ss << "C:\\Users\\tnkt\\Documents\\Visual Studio 2013\\Projects\\MasterWorkerProcessTest\\Debug\\Temp\\" << fileName << endl;
+	std::ifstream ifs(ss.str(), std::ifstream::binary);
+	if (ifs.fail())
+	{
+		std::cerr << "失敗" << std::endl;
+	}
+	else
+	{
+		std::string str((std::istreambuf_iterator<char>(ifs)),
+			std::istreambuf_iterator<char>());
+		cout << "file content: " << str << endl;
+	}
+
+}
+
+void writeFileTest()
+{
+	ofstream fout("C:\\Users\\tnkt\\Documents\\Visual Studio 2013\\Projects\\MasterWorkerProcessTest\\Debug\\Temp\\lowlevelfile.txt");
+	//ofstream fout("C:\\Users\\tnkt\\Documents\\Visual Studio 2013\\Projects\\MasterWorkerProcessTest\\Debug\\Temp\\middlelevelfile.txt");
+	if (fout.fail()){  // if(!fout)でもよい。
+		cout << "出力ファイルをオープンできません" << endl;
+	}
+	else
+	{
+		fout << "write file" << endl;
+	}
 }
 
 void createFileTest()
 {
 	char str[] = "hogehoge";
 	stringstream ss;
-	ss << "C://CPP//" << "sample" << 0 << ".txt";
+	//ss << "C://CPP//" << "sample" << 0 << ".txt";
+	ss << "C:\\Users\\tnkt\\Documents\\Visual Studio 2013\\Projects\\MasterWorkerProcessTest\\Debug\\Temp\\" << "sample" << ".txt";
 	string path = ss.str();
 	HANDLE hFile = CreateFile(TEXT(path.c_str()), GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
 	DWORD dwWriteSize;
@@ -73,20 +109,25 @@ void exePythonTest()
 {
 #ifdef UP
 	cout << "init python" << endl;
-	string dums;
-	getline(cin, dums);
 	Py_Initialize();
 
 
 	cout << "run python" << endl;
-	PyRun_SimpleString("import json\n"
-		"print 'imported json'\n");
+	PyRun_SimpleString(
+		//"import json\n"
+		//"print 'imported json'\n"
+		"f = open('pythontest.txt', 'w')\n"
+		//"f = open('pythontest.txt','r')\n"
+		//"for row in f:\n"
+		//"\tprint row\n"
+		//"f = open('C:\\CPP\\')"
+		"f.write('I am python')\n"
+		"f.close()\n");
 	//PyRun_SimpleString("print 'hogehoge'");
-	getline(cin, dums);
 	cout << "python executed" << endl;
 	Py_Finalize();
 	cout << "python finalized" << endl;
-	getline(cin, dums);
+	waitEnter();
 #endif
 }
 
@@ -136,6 +177,43 @@ HANDLE createRestrictedToken(HANDLE hPreToken, DWORD flags, DWORD deletePrivileg
 	return createRestrictedToken(hPreToken, flags, 0, nullptr, deletePrivilegeCount, privilegeToDelete);
 }
 
+
+
+enum IntegrityLevel {
+	INTEGRITY_LEVEL_SYSTEM,
+	INTEGRITY_LEVEL_HIGH,
+	INTEGRITY_LEVEL_MEDIUM,
+	INTEGRITY_LEVEL_MEDIUM_LOW,
+	INTEGRITY_LEVEL_LOW,
+	INTEGRITY_LEVEL_BELOW_LOW,
+	INTEGRITY_LEVEL_UNTRUSTED,
+	INTEGRITY_LEVEL_LAST
+};
+
+const LPCTSTR GetIntegrityLevelString(IntegrityLevel integrity_level) {
+	switch (integrity_level) {
+	case INTEGRITY_LEVEL_SYSTEM:
+		return "S-1-16-16384";
+	case INTEGRITY_LEVEL_HIGH:
+		return "S-1-16-12288";
+	case INTEGRITY_LEVEL_MEDIUM:
+		return "S-1-16-8192";
+	case INTEGRITY_LEVEL_MEDIUM_LOW:
+		return "S-1-16-6144";
+	case INTEGRITY_LEVEL_LOW:
+		return "S-1-16-4096";
+	case INTEGRITY_LEVEL_BELOW_LOW:
+		return "S-1-16-2048";
+	case INTEGRITY_LEVEL_UNTRUSTED:
+		return "S-1-16-0";
+	case INTEGRITY_LEVEL_LAST:
+		return NULL;
+	}
+	return NULL;
+}
+
+
+
 int main(void)
 {
 	//始めのトークンを取得
@@ -171,18 +249,71 @@ int main(void)
 	auto pTokenPrivileges = getTokenInformation<PTOKEN_PRIVILEGES>(hTokenGroupsRestricted, TokenPrivileges);
 	auto hTokenPrivilegesRestricted = createRestrictedToken(hTokenGroupsRestricted, DISABLE_MAX_PRIVILEGE, pTokenPrivileges->PrivilegeCount, pTokenPrivileges->Privileges);
 
+	//
+	//chrome restricted_token_utils
+	//
+
+	//const wchar*からLPCTSTRに書き換えた（良いのか？）
+	LPCTSTR  integrity_level_str = GetIntegrityLevelString(INTEGRITY_LEVEL_LOW);
+	waitEnter("convert");
+	PSID integrity_sid = nullptr;
+	//LPCSTRに変えないとここでコンパイルエラー
+	if (!::ConvertStringSidToSid(integrity_level_str, &integrity_sid))
+		throw std::exception("convert string sid to sid error");
+
+	TOKEN_MANDATORY_LABEL label;
+	label.Label.Attributes = SE_GROUP_INTEGRITY;
+	//label.Label.Attributes = SE_GROUP_INTEGRITY_ENABLED;
+	label.Label.Sid = integrity_sid;
+
+	//msdnのマネ
+	HANDLE duplicatedToken;
+	DuplicateTokenEx(hInitialToken, MAXIMUM_ALLOWED, nullptr, SecurityImpersonation, TokenPrimary, &duplicatedToken);
+
+	DWORD size = sizeof(TOKEN_MANDATORY_LABEL) + ::GetLengthSid(integrity_sid);
+	//制限をつけてないトークンにやる
+	BOOL result = ::SetTokenInformation(duplicatedToken, TokenIntegrityLevel, &label, size);
+	//bool result = false;
+	::LocalFree(integrity_sid);
+	if (!result)
+	{
+		auto error = GetLastError();
+		const std::exception ex("Failed to SetTokenInformation");
+		throw &ex;
+	}
+	//
+	// end chrome source code
+	//
+
 	cout << "last privileges" << endl;
 	OutputPirivileges(pTokenPrivileges);
 
-	ImpersonateLoggedOnUser(hTokenPrivilegesRestricted);
+	//色々剥奪したトークンをプロセスに適用(偽装)
+	//ImpersonateLoggedOnUser(hTokenPrivilegesRestricted);
+	ImpersonateLoggedOnUser(duplicatedToken);
+	//ImpersonateLoggedOnUser(hTokenUserRestricted);
 
-	WinExec("notepad", SW_SHOW);
+	//ファイル読み込みテスト
+	std::ifstream ifs("C:\\Users\\tnkt\\Documents\\Visual Studio 2013\\Projects\\MasterWorkerProcessTest\\Debug\\Temp\\lowlevelfile.txt", std::ifstream::binary);
+	if (ifs.fail())
+		std::cerr << "失敗" << std::endl;
+	else
+	{
+		std::string str((std::istreambuf_iterator<char>(ifs)),
+			std::istreambuf_iterator<char>());
+		cout << "file content: " << str << endl;
+	}
+	//ファイル読み込みテスト
+
+	writeFileTest();
+	//WinExec("notepad", SW_SHOW);
 
 	waitEnter();
 
 	exePythonTest();
 	createFileTest();
 
+	//トークンを戻す
 	RevertToSelf();
 
 	//解放
